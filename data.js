0fbb0b5acf2ccd159f0fc24d4e21fbda732b0f79
Shirakami_Cocoa6078 <71052778+ShirakamiCocoa6078@users.noreply.github.com>
2023-07-13 11:00:01 +0000

    asdf

--- data.js
+++ data.js
@@ -100,7 +100,7 @@ function order(){
   ${resultValue}
   -----------------------------
   `
-  document.getElementById("ordered").appendChild(h2);
+  document.getElementById("ordered").prepend(h2);
   order_count += 1;
   for(var i=1;i<=12;i++){
     document.getElementById(`value${i}`).innerText = 0;
